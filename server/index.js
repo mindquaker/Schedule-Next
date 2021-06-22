6b3b34859440a9a643e318d6930f43c03dc68ae6
unknown <mindquaker20@gmail.com>
2021-06-21 23:20:14 -0400

    u[dated index.js in server

--- server/index.js
+++ server/index.js
@@ -4,7 +4,13 @@ const PORT = 3001;
 
 const app = express();
 
+const proxy = require("http-proxy-middleware");
 
+module.exports = function(app) {
+  app.use(
+    proxy(["/api", , "/otherApi"], { target: "http://localhost:3001" })
+  );
+};
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
