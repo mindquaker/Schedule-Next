c6e2bf5f8a5850e3bc3f2312a6e0b556fb9c3617
unknown <mindquaker20@gmail.com>
2021-06-22 11:12:47 -0400

    added props in Carousel for platfrom name and logo

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -3,16 +3,17 @@
 import React from "react";
 import "../App.css";
 import { Card, Carousel } from "react-bootstrap";
-import Zoom from "./zoom.png";
-export function Cards() {
+export function Cards(props) {
+  const  {platform,
+  logo} = props;
   return (
     <div>
       <Card bg={"primary"} style={{ width: "18rem" }} className='mb-2'>
             <Card.Header>
-              <img src={Zoom} alt='zoom logo' height='42' width='42'></img>
+              <img src={logo} alt='zoom logo' height='42' width='42'></img>
             </Card.Header>
             <Card.Body>
-              <Card.Title> Zoom Meeting </Card.Title>
+              <Card.Title> {platform} </Card.Title>
               <Card.Text>
                 Power your voice communications with our global cloud phone
                 solution with secure call routing, call queues, SMS, elevate
--- client/src/components/Home-body.js
+++ client/src/components/Home-body.js
@@ -1,9 +1,15 @@
 /* eslint-disable */
 import React from "react";
 import "../App.css";
-import { Button, Card,  Carousel } from "react-bootstrap";
+import { Button,  Carousel } from "react-bootstrap";
 import { Cards } from "./Cards";
-
+import Zoom from "./zoom.png";
+import Webex from "./webex.png";
+import Meet from "./google-meet.png";
+import MsTeam from "./ms-team.png";
+import Skype from './skype.png'
+import Goto from "./gotomeeting.png";
+import More from "./more.png"
 export function HomeBody() {
   return (
     <div>
@@ -15,22 +21,29 @@ export function HomeBody() {
       </p>
     
       <Carousel>
-        <Carousel.Item interval={1000}>
-          <Cards />
+        <Carousel.Item interval={800}>
+          <Cards  platform = "Zoom Meeting" logo = {Zoom}/>
         </Carousel.Item>
-        <Carousel.Item interval={500}>
-         <Cards />
+        <Carousel.Item interval={800}>
+         <Cards platform = "Webex Meeting" logo = {Webex}/>
         </Carousel.Item>
-        <Carousel.Item>
-         <Cards />
+        <Carousel.Item interval={800}>
+         <Cards platform = "Google Meet" logo = {Meet}/>
         </Carousel.Item>
-        <Carousel.Item>
-         <Cards />
+        <Carousel.Item interval={800}>
+         <Cards platform = "Skype" logo = {Skype}/>
+        </Carousel.Item >
+        <Carousel.Item interval={800}>
+         <Cards platform="Microsoft Team" logo = {MsTeam}/>
         </Carousel.Item>
-        <Carousel.Item>
-         <Cards />
+        <Carousel.Item interval={800}>
+         <Cards platform="GoToMeeting" logo = {Goto}/>
+        </Carousel.Item>
+        <Carousel.Item interval={800}>
+         <Cards platform="Many More" logo = {More}/>
         </Carousel.Item>
       </Carousel>
+      
       <div class='buttons'>
         <Button variant='primary'>Login</Button>
         <h3 class='or'>OR</h3>
